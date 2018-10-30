0f60977ab89abeb8b0e7638f26d502fcb5abc8ab
Anvesh Chintu <Anvesh1305@gmail.com>
2018-10-29 21:58:19 -0400

    Add delete Todos

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
     
-    
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
         return res.send(400).send();
     }
@@ -46,6 +45,20 @@ app.post('/todos', function(req, res){
     todos.push(body);
     
     res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if(!matchedTodo){
+        res.status(404).json({"error": "Id not found"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+    
 });
 
 app.listen(PORT, function () {
