2fdfe812d114ea197fa43b6016f9bd6bcea84de9
Anvesh Chintu <Anvesh1305@gmail.com>
2018-11-07 12:07:39 -0500

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -17,47 +17,41 @@ var Todo = sequelize.define('todo', {
 		allowNull: false,
 		defaultValue: false
 	}
-})
+});
+
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
 
 sequelize.sync({
 	//force: true
 }).then(function(){
 	console.log('Everything is synced');
 
+	User.findById(1).then(function(user){
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function(todos){
+			todos.forEach(function(todo){
+				console.log(todo.toJSON());
+			})
+		})
+	})
 
-	Todo.findById(2).then(function(todo){
-		if(todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('Todo Not Found');
-		}
-	});
-	
-
-	// Todo.create({
-	// 	description: 'Work hard till win'
-	// }).then(function(todo){
+	// User.create({
+	// 	email: 'Anvesh@example.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: 'Stay Fit'
+	// 		description: 'Finish Node.js'
 	// 	});
-	// }).then(function(){
-	// 	//return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%fit%'
-	// 			}
-	// 		}
+	// }).then(function(todo){
+	// 	User.findById(1).then(function(user){
+	// 		user.addTodo(todo);
 	// 	});
-	// }).then(function(todos){
-	// 	if(todos){
-	// 		todos.forEach(function(todo){
-	// 			console.log(todo.toJSON());
-	// 		});
-	// 	} else {
-	// 		console.log('No TODO found!');
-	// 	}
-	// }).catch(function(e){
-	// 	console.log(e);
 	// });
 });
